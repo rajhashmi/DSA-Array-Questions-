3307f454845f54bf5bde621b51c648a1135b9c26
shahid <shahidhashmiraj@gmail.com>
2023-09-10 19:52:15 +0530

    four Sum brute force

--- app.js
+++ app.js
@@ -720,3 +720,30 @@ function ThreSum_better(nums){
 
 }
 console.log(ThreSum_better( [-1, 0, 1, 2, -1, -4]));
+
+
+
+
+//  four sum 
+//  brute force
+
+function FourSum(nums){
+  let result = [];
+  let n = nums.length;
+
+  for(let i = 0; i < n; i++){
+    for(let j = i+1; j < n;j++){
+      for(let k = j+1; k < n; k++){
+        for(let l = k+1; l < n; l++){
+          if(nums[i] + nums[j] + nums[k] + nums[l] == 0){
+            result.push([nums[i],nums[j], nums[k], nums[l]].sort((a,b)=> a-b))
+          }
+        }
+      }
+    }
+  }
+  let set = new Set(result.map(JSON.stringify));
+  result = Array.from(set).map(JSON.parse);
+  return result
+}
+console.log(FourSum([1,0,-1,0,-2,2]))
